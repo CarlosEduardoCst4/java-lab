38c632f0570a1da05fff2158745d4901b2dacdcf
Carlos Eduardo Costa <dudu.cst4@gmail.com>
2026-09-17 16:53:39 -0300

    novos arquivos

---
+++ AreaCircunferencia/src/main/java/Streams/carro/Map.java
@@ -0,0 +1,23 @@
+package Streams.carro;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
+
+public class Map {
+
+    static void main() {
+        Consumer<String> print = System.out::print;
+
+        List<String> marcas = Arrays.asList("Bmw ", "Audi ", "Honda ");
+        marcas.stream().map(m -> m.toUpperCase()).forEach(print);
+
+        UnaryOperator<String> primeiraLetra = n -> n.charAt(0) + "";
+
+        System.out.println("\n\nUsando composição...");
+        marcas.stream().map(Utilitarios.maiuscula).map(primeiraLetra).map(Utilitarios::grito).forEach(print);
+    }
+
+
+}
---
+++ AreaCircunferencia/src/main/java/Streams/carro/Utilitarios.java
@@ -0,0 +1,19 @@
+package Streams.carro;
+
+import java.util.function.UnaryOperator;
+
+public class Utilitarios {
+
+    private Utilitarios(){
+
+    }
+
+    public final static UnaryOperator<String> maiuscula = n -> n.toUpperCase();
+
+    public final static UnaryOperator<String> primeiraletra = n -> n.charAt(0) + "";
+
+    public final static String grito(String n){
+        return n + "!!!";
+    }
+
+}
